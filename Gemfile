2e99adc36a30100dda93ca67e507145111ae9e94
Iain Barnett <iainspeed@gmail.com>
2013-08-17 18:47:51 +0100

    Fixed up the versions.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rack-lodash.gemspec
 gemspec
+
+group :development do
+  gem "wirble"
+end
--- lib/rack/lodash/version.rb
+++ lib/rack/lodash/version.rb
@@ -1,5 +1,14 @@
 module Rack
   module Lodash
+
+    # The version of this library.
     VERSION = "0.0.1"
+
+    # The version of Lo-dash it supports
+    LODASH_VERSION = "1.3.1"
+
+    # This is the release date of the Lo-dash file, it makes an easy "Last-Modified" date for setting the headers around caching.
+    # @todo remember to change Last-Modified with each release!
+    LODASH_VERSION_DATE = "Wed, 12 Jun 2013 15:54:39 +0100"
   end
 end
