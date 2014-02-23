4134a9975261ae17187ab2120eea6b45b74c3938
Iain Barnett <iainspeed@gmail.com>
2014-02-23 01:23:11 +0000

    Updated to v2.4.1 of Lo-dash.

--- lib/rack/lodash/version.rb
+++ lib/rack/lodash/version.rb
@@ -5,10 +5,10 @@ module Rack
     VERSION = "1.1.0"
 
     # The version of Lo-dash it supports
-    LODASH_VERSION = "2.2.1"
+    LODASH_VERSION = "2.4.1"
 
     # This is the release date of the Lo-dash file, it makes an easy "Last-Modified" date for setting the headers around caching.
-    # @todo remember to change Last-Modified with each release! To get this, DateTime.parse(release_date).rfc2822
-    LODASH_VERSION_DATE = "Thu, 3 Oct 2013 09:42:50 +0000"
+    # @todo remember to change Last-Modified with each release! To get this, Time.parse(release_date).rfc2822
+    LODASH_VERSION_DATE = "Sun, 01 Dec 2013 17:37:28 +0000"
   end
 end
